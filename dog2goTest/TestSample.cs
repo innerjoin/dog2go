5572f7b7df2793af52f704d51592adee34c101a0
lukasst <lukas.steiger@hotmail.com>
2016-03-02 11:53:20 +0100

    seperate test project added

---
+++ dog2goTest/TestSample.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace dog2goTest
+{
+    [TestFixture]
+    public class TestSample
+    {
+        [Test]
+        public void JustAHappyTest()
+        {
+            Assert.That(true, Is.EqualTo(true));    
+        }
+    }
+}
