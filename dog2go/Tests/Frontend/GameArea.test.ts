955437e4cfdd591d168e33931db208a7303b7b98
Lukas Steiger <lukas.steiger@outlook.com>
2016-04-13 11:27:16 +0200

    move kennelfields from left to right

--- dog2go/Tests/Frontend/GameArea.test.ts
+++ dog2go/Tests/Frontend/GameArea.test.ts
@@ -31,7 +31,7 @@ describe("GameArea", () => {
         var xStart = [380, 20, 140, 500];
         var yStart = [20, 140, 500, 380];
         var fc = new FieldCoordinatesData(span, xStart, yStart);
-        var ac = new Coordinates.AreaCoordinates(1, fc);
+        var ac = new Coordinates.AreaCoordinates(0, fc);
         setTimeout(() => {
             area.addKennelFields(game, data, ac, 0xFF00CC);
         }, 0);
@@ -40,13 +40,14 @@ describe("GameArea", () => {
         var pos1 = data[1].viewRepresentation.position;
         var pos2 = data[2].viewRepresentation.position;
         var pos3 = data[3].viewRepresentation.position;
-        expect(pos0.x).toEqual(20);
+        console.log(pos0, pos1, pos2, pos3);
+        expect(pos0.x).toEqual(50);
         expect(pos0.y).toEqual(20);
         expect(pos1.x).toEqual(20);
-        expect(pos1.y).toEqual(50);
+        expect(pos1.y).toEqual(20);
         expect(pos2.x).toEqual(50);
-        expect(pos2.y).toEqual(20);
-        expect(pos3.x).toEqual(50);
+        expect(pos2.y).toEqual(50);
+        expect(pos3.x).toEqual(20);
         expect(pos3.y).toEqual(50);
     }, 10000);
 });
