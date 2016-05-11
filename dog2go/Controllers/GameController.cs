6e21f0967e04d23999755f19f26d7adf1dd8cecf
Yanick Gubler <ygubler@hsr.ch>
2016-05-11 14:48:00 +0200

    angefangen ChooseGameTable zu implementieren

--- dog2go/Controllers/GameController.cs
+++ dog2go/Controllers/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using System.Web.Security;
 using dog2go.Backend.Model;
 using dog2go.Backend.Repos;
 
@@ -24,6 +26,21 @@ namespace dog2go.Controllers
             ConcurrentDictionary<string, User> users = UserRepository.Instance.Get();
             if (users == null || users.Count == 0) return true;
             return users.FirstOrDefault(x => x.Value.Identifier == User.Identity.Name).Value == null;
+        }
+
+        public ActionResult ChooseGameTable()
+        {
+            List<GameTable> gameTableList = GameRepository.Instance.Get();
+            return View(gameTableList);
+        }
+
+        [HttpGet]
+        [ActionName("GetTables")]
+        public ActionResult GetTables()
+        {
+            List<GameTable> gamesList = GameRepository.Instance.Get();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
         }
     }
 }
---
+++ dog2go/Models/ChooseTableViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace dog2go.Models
+{
+    public class ChooseTableViewModels
+    {
+        [Required(ErrorMessage = "No GameTable exists")]
+        //[Display(Name = "User name")]
+        public List<int> GameTableIdList { get; set; }
+        public int GameTableId { get; set; }
+
+    }
+}
