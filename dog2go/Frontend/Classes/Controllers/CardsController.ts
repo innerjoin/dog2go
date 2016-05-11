c1769ed21b04c5fc31def294b89d54e20850d486
Lukas Steiger <lukas.steiger@outlook.com>
2016-05-11 11:16:03 +0200

    texte, schriften und co angepasst
    Related Work Items: #319

--- dog2go/Frontend/Classes/Controllers/CardsController.ts
+++ dog2go/Frontend/Classes/Controllers/CardsController.ts
@@ -48,22 +48,12 @@ export class CardsController {
                 $(`#${cards[i].Name}`).draggable({
                     // keep this as a function, else the scope gets broken.
                     revert: function (event, ui) {
-                        // on older version of jQuery use "draggable"
-                        // $(this).data("draggable")
-                        // 
-                        // on 1.11.x versions of jQuery use "uiDraggable"
-                        // $(this).data("uiDraggable")
-                        // on 2.x versions of jQuery use "ui-draggable"
-                        // $(this).data("ui-draggable")
                         console.log($(this).data("ui-draggable"));
                         $(this).data("ui-draggable").originalPosition = {
                             top: 0,
                             left: 0
                         };
-                        // return boolean
                         return !event;
-                        // that evaluate like this:
-                        // return event !== false ? false : true;
                     }
                 });
             }
