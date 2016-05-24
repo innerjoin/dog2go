1c0c276f6eeb2618f07b5308bb6ba49a57fee3f2
Lukas Steiger <lukas.steiger@outlook.com>
2016-05-24 13:20:46 +0200

    fix playerFieldArea bugs and add tests
    Related Work Items: #342

--- dog2go/Backend/Model/PlayerFieldArea.cs
+++ dog2go/Backend/Model/PlayerFieldArea.cs
@@ -1,32 +1,30 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Web.UI.WebControls;
 
 namespace dog2go.Backend.Model
 {
     public class PlayerFieldArea
     {
-        private const int NumberOfMeeple = 4;
+        private const int NumberOfMeeple = 4; // defines also the number of kennels and end fields
         private const int NumberOfFieldsBeforeStart = 4;
         private const int NumberOfFieldsAfterStart = 11;
 
-        private int _previousIdentifier;
-        private int _nextIdentifier;
-        public int PreviousIdentifier => _previousIdentifier;
-        public int NextIdentifier => _nextIdentifier;
-        public int Identifier { get; }
-        public int FieldId { get; private set; }
         private PlayerFieldArea _previous;
         private PlayerFieldArea _next;
-        public ColorCode ColorCode { get; set; }
-        public List<MoveDestinationField> Fields { get; set; }
-        public List<KennelField> KennelFields { get; set; }
-        public List<EndField> EndFields { get; set; }
-        public StartField StartField { get; set; }
-        public List<Meeple> Meeples { get; set; }
+
+        public int PreviousIdentifier { get; private set; }
+        public int NextIdentifier { get; private set; }
+
+        public int Identifier { get; }
+        public int FieldId { get; private set; }
+
+        public ColorCode ColorCode { get; }
+        public List<MoveDestinationField> Fields { get; private set; }
+        public List<KennelField> KennelFields { get; private set; }
+        public List<EndField> EndFields { get; private set; }
+        public StartField StartField { get; private set; }
+        public List<Meeple> Meeples { get; private set; }
         public Participation Participation { get; set; }
         [IgnoreDataMember]
         public PlayerFieldArea Previous
@@ -36,9 +34,9 @@ namespace dog2go.Backend.Model
             {
                 _previous = value;
                 if (value == null) return;
-                _previousIdentifier = value.Identifier;
-                value.Fields.Last(field => field.FieldType.Contains("StandardField")).Next = this.Fields.First(field => field.FieldType.Contains("StandardField"));
-                this.Fields.First(field => field.FieldType.Contains("StandardField")).Previous = value.Fields.Last(field => field.FieldType.Contains("StandardField"));
+                PreviousIdentifier = value.Identifier;
+                value.Fields.Last(field => field.FieldType.Contains("StandardField")).Next = Fields.First(field => field.FieldType.Contains("StandardField"));
+                Fields.First(field => field.FieldType.Contains("StandardField")).Previous = value.Fields.Last(field => field.FieldType.Contains("StandardField"));
             }
         }
 
@@ -50,9 +48,9 @@ namespace dog2go.Backend.Model
             {
                 _next = value;
                 if (value == null) return;
-                _nextIdentifier = value.Identifier;
-                value.Fields.First(field => field.FieldType.Contains("StandardField")).Previous = this.Fields.Last(field => field.FieldType.Contains("StandardField"));
-                this.Fields.Last(field => field.FieldType.Contains("StandardField")).Next = value.Fields.First(field => field.FieldType.Contains("StandardField"));
+                NextIdentifier = value.Identifier;
+                value.Fields.First(field => field.FieldType.Contains("StandardField")).Previous = Fields.Last(field => field.FieldType.Contains("StandardField"));
+                Fields.Last(field => field.FieldType.Contains("StandardField")).Next = value.Fields.First(field => field.FieldType.Contains("StandardField"));
             }
         }
 
@@ -70,11 +68,11 @@ namespace dog2go.Backend.Model
             List<KennelField> kennelFields = new List<KennelField>();
             List<EndField> endFields = new List<EndField>();
 
-            for (var count = 0; count < NumberOfMeeple; count++)
+            for (int count = 0; count < NumberOfMeeple; count++)
             {
                 KennelField kennelField = new KennelField(++fieldId);
                 int meepleId = (Identifier - 1) * NumberOfMeeple + count;
-                Meeple newMeeple = new Meeple(kennelField, this.ColorCode, meepleId) {IsStartFieldBlocked = true};
+                Meeple newMeeple = new Meeple(kennelField, ColorCode, meepleId) {IsStartFieldBlocked = true};
                 meepleList.Add(newMeeple);
                 kennelField.CurrentMeeple = newMeeple;
                 kennelFields.Add(kennelField);
@@ -83,7 +81,7 @@ namespace dog2go.Backend.Model
             List<MoveDestinationField> fields = new List<MoveDestinationField>();
             StandardField standardField = new StandardField(++fieldId) { Previous = null };
 
-            for (var count = 1; count < NumberOfFieldsBeforeStart; count++)
+            for (int count = 1; count < NumberOfFieldsBeforeStart; count++)
             {
                 StandardField tempField = new StandardField(++fieldId) { Previous = standardField };
                 standardField.Next = tempField;
@@ -91,28 +89,20 @@ namespace dog2go.Backend.Model
                 standardField = tempField;
             }
 
-            StartField startField = new StartField(++fieldId, this.ColorCode);
+            StartField startField = new StartField(++fieldId, ColorCode);
             standardField.Next = startField;
             startField.Previous = standardField;
 
             fields.Add(standardField);
-
-            EndField endField = new EndField(fieldId + NumberOfFieldsAfterStart + 2) { Previous = startField };
-            startField.EndFieldEntry = endField;
-            
 
             StandardField standardFieldAfter = new StandardField(++fieldId) { Previous = startField };
             startField.Next = standardFieldAfter;
             fields.Add(startField);
             StartField = startField;
 
-            for (int count = 1; count <= NumberOfFieldsAfterStart; count++)
+            for (int count = 1; count < NumberOfFieldsAfterStart; count++)
             {
                 StandardField tempField = new StandardField(++fieldId) { Previous = standardFieldAfter };
-                if (count == 1)
-                {
-                    StartField.Next = tempField;
-                }
                 standardFieldAfter.Next = tempField;
                 fields.Add(standardFieldAfter);
                 standardFieldAfter = tempField;
@@ -120,8 +110,10 @@ namespace dog2go.Backend.Model
 
             fields.Add(standardFieldAfter);
 
-            ++fieldId;
-            for (int count = 1; count < NumberOfFieldsBeforeStart; count++)
+            EndField endField = new EndField(++fieldId) { Previous = startField };
+            startField.EndFieldEntry = endField;
+
+            for (int count = 1; count < NumberOfMeeple; count++)
             {
                 EndField tempEndField = new EndField(++fieldId) { Previous = endField };
                 endField.Next = tempEndField;
---
+++ dog2go/Tests/Backend/PlayerFieldAreaTest.cs
@@ -0,0 +1,195 @@
+﻿using dog2go.Backend.Model;
+using NUnit.Framework;
+
+namespace dog2go.Tests.Backend
+{
+    [TestFixture]
+    public class PlayerFieldAreaTest
+    {
+        private const int Identifier = 1;
+        private const int FieldId = 0;
+        private const ColorCode Color = ColorCode.Green;
+        private PlayerFieldArea _pf;
+
+        [SetUp]
+        public void CreateAreaForTest()
+        {
+            _pf = new PlayerFieldArea(Identifier, Color, FieldId);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_Generation()
+        {
+            // test area identification
+            Assert.AreEqual(Identifier, _pf.Identifier);
+            // test color set
+            Assert.AreEqual(Color, _pf.ColorCode);
+            // test the count of all fields (except kennel)
+            Assert.AreEqual(20, _pf.Fields.Count);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_Kennels()
+        {
+            // now test the kennel fields identifier
+            Assert.AreEqual(4, _pf.KennelFields.Count);
+            Assert.AreEqual(1, _pf.KennelFields[0].Identifier);
+            Assert.AreEqual(2, _pf.KennelFields[1].Identifier);
+            Assert.AreEqual(3, _pf.KennelFields[2].Identifier);
+            Assert.AreEqual(4, _pf.KennelFields[3].Identifier);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_StandardFieldsBefore()
+        {
+            Assert.AreEqual(5, _pf.Fields[0].Identifier);
+            Assert.AreEqual(6, _pf.Fields[1].Identifier);
+            Assert.AreEqual(7, _pf.Fields[2].Identifier);
+            Assert.AreEqual(8, _pf.Fields[3].Identifier);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_StartField()
+        {
+            Assert.AreEqual(9, _pf.Fields[4].Identifier);
+            Assert.IsInstanceOf<StartField>(_pf.Fields[4]);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_StandardFieldsAfter()
+        {
+            Assert.AreEqual(10, _pf.Fields[5].Identifier);
+            Assert.AreEqual(11, _pf.Fields[6].Identifier);
+            Assert.AreEqual(12, _pf.Fields[7].Identifier);
+            Assert.AreEqual(13, _pf.Fields[8].Identifier);
+            Assert.AreEqual(14, _pf.Fields[9].Identifier);
+            Assert.AreEqual(15, _pf.Fields[10].Identifier);
+            Assert.AreEqual(16, _pf.Fields[11].Identifier);
+            Assert.AreEqual(17, _pf.Fields[12].Identifier);
+            Assert.AreEqual(18, _pf.Fields[13].Identifier);
+            Assert.AreEqual(19, _pf.Fields[14].Identifier);
+            Assert.AreEqual(20, _pf.Fields[15].Identifier);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_EndFields()
+        {
+            Assert.AreEqual(21, _pf.Fields[16].Identifier);
+            Assert.AreEqual(22, _pf.Fields[17].Identifier);
+            Assert.AreEqual(23, _pf.Fields[18].Identifier);
+            Assert.AreEqual(24, _pf.Fields[19].Identifier);
+            Assert.IsInstanceOf<EndField>(_pf.Fields[16]);
+            Assert.IsInstanceOf<EndField>(_pf.Fields[17]);
+            Assert.IsInstanceOf<EndField>(_pf.Fields[18]);
+            Assert.IsInstanceOf<EndField>(_pf.Fields[19]);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_Next()
+        {
+            // standard fields before start
+            Assert.AreEqual(_pf.Fields[1], _pf.Fields[0].Next);
+            Assert.AreEqual(_pf.Fields[2], _pf.Fields[1].Next);
+            Assert.AreEqual(_pf.Fields[3], _pf.Fields[2].Next);
+            // next is startfield, both test the same jump
+            Assert.AreEqual(_pf.Fields[4], _pf.Fields[3].Next);
+            Assert.AreEqual(_pf.StartField, _pf.Fields[3].Next);
+            // next is after start field, both test teh same jump
+            Assert.AreEqual(_pf.Fields[5], _pf.Fields[4].Next);
+            Assert.AreEqual(_pf.Fields[5], _pf.StartField.Next);
+            // other standard fields after start
+            Assert.AreEqual(_pf.Fields[6], _pf.Fields[5].Next);
+            Assert.AreEqual(_pf.Fields[7], _pf.Fields[6].Next);
+            Assert.AreEqual(_pf.Fields[8], _pf.Fields[7].Next);
+            Assert.AreEqual(_pf.Fields[9], _pf.Fields[8].Next);
+            Assert.AreEqual(_pf.Fields[10], _pf.Fields[9].Next);
+            Assert.AreEqual(_pf.Fields[11], _pf.Fields[10].Next);
+            Assert.AreEqual(_pf.Fields[12], _pf.Fields[11].Next);
+            Assert.AreEqual(_pf.Fields[13], _pf.Fields[12].Next);
+            Assert.AreEqual(_pf.Fields[14], _pf.Fields[13].Next);
+            Assert.AreEqual(_pf.Fields[15], _pf.Fields[14].Next);
+            Assert.AreEqual(null, _pf.Fields[15].Next);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_NextByIdentifier()
+        {
+            // standard fields before start
+            Assert.AreEqual(6, _pf.Fields[0].Next.Identifier);
+            Assert.AreEqual(7, _pf.Fields[1].Next.Identifier);
+            Assert.AreEqual(8, _pf.Fields[2].Next.Identifier);
+            // next is startfield, both test the same jump
+            Assert.AreEqual(9, _pf.Fields[3].Next.Identifier);
+            Assert.AreEqual(9, _pf.Fields[3].Next.Identifier);
+            // next is after start field, both test teh same jump
+            Assert.AreEqual(10, _pf.Fields[4].Next.Identifier);
+            Assert.AreEqual(10, _pf.StartField.Next.Identifier);
+            // other standard fields after start
+            Assert.AreEqual(11, _pf.Fields[5].Next.Identifier);
+            Assert.AreEqual(12, _pf.Fields[6].Next.Identifier);
+            Assert.AreEqual(13, _pf.Fields[7].Next.Identifier);
+            Assert.AreEqual(14, _pf.Fields[8].Next.Identifier);
+            Assert.AreEqual(15, _pf.Fields[9].Next.Identifier);
+            Assert.AreEqual(16, _pf.Fields[10].Next.Identifier);
+            Assert.AreEqual(17, _pf.Fields[11].Next.Identifier);
+            Assert.AreEqual(18, _pf.Fields[12].Next.Identifier);
+            Assert.AreEqual(19, _pf.Fields[13].Next.Identifier);
+            Assert.AreEqual(20, _pf.Fields[14].Next.Identifier);
+            Assert.AreEqual(null, _pf.Fields[15].Next);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_NextEndField()
+        {
+            Assert.AreEqual(_pf.EndFields[0], _pf.StartField.EndFieldEntry);
+            Assert.AreEqual(_pf.EndFields[1], _pf.EndFields[0].Next);
+            Assert.AreEqual(_pf.EndFields[2], _pf.EndFields[1].Next);
+            Assert.AreEqual(_pf.EndFields[3], _pf.EndFields[2].Next);
+            Assert.AreEqual(null, _pf.EndFields[3].Next);
+            Assert.AreEqual(21, _pf.StartField.EndFieldEntry.Identifier);
+            Assert.AreEqual(22, _pf.EndFields[0].Next.Identifier);
+            Assert.AreEqual(23, _pf.EndFields[1].Next.Identifier);
+            Assert.AreEqual(24, _pf.EndFields[2].Next.Identifier);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_Previous()
+        {
+            // standard fields before start
+            Assert.AreEqual(null, _pf.Fields[0].Previous);
+            Assert.AreEqual(_pf.Fields[0], _pf.Fields[1].Previous);
+            Assert.AreEqual(_pf.Fields[1], _pf.Fields[2].Previous);
+            Assert.AreEqual(_pf.Fields[2], _pf.Fields[3].Previous);
+
+            Assert.AreEqual(_pf.Fields[3], _pf.Fields[4].Previous);
+            Assert.AreEqual(_pf.Fields[3], _pf.StartField.Previous);
+
+            Assert.AreEqual(_pf.Fields[4], _pf.Fields[5].Previous);
+            Assert.AreEqual(_pf.StartField, _pf.Fields[5].Previous);
+
+            Assert.AreEqual(_pf.Fields[5], _pf.Fields[6].Previous);
+            Assert.AreEqual(_pf.Fields[6], _pf.Fields[7].Previous);
+            Assert.AreEqual(_pf.Fields[7], _pf.Fields[8].Previous);
+            Assert.AreEqual(_pf.Fields[8], _pf.Fields[9].Previous);
+            Assert.AreEqual(_pf.Fields[9], _pf.Fields[10].Previous);
+            Assert.AreEqual(_pf.Fields[10], _pf.Fields[11].Previous);
+            Assert.AreEqual(_pf.Fields[11], _pf.Fields[12].Previous);
+            Assert.AreEqual(_pf.Fields[12], _pf.Fields[13].Previous);
+            Assert.AreEqual(_pf.Fields[13], _pf.Fields[14].Previous);
+            Assert.AreEqual(_pf.Fields[14], _pf.Fields[15].Previous);
+        }
+
+        [Test]
+        public void TestPlayerFieldArea_PreviousEndField()
+        {
+            Assert.AreEqual(_pf.StartField, _pf.EndFields[0].Previous);
+            Assert.AreEqual(_pf.EndFields[0], _pf.EndFields[1].Previous);
+            Assert.AreEqual(_pf.EndFields[1], _pf.EndFields[2].Previous);
+            Assert.AreEqual(_pf.EndFields[2], _pf.EndFields[3].Previous);
+            Assert.AreEqual(9, _pf.EndFields[0].Previous.Identifier);
+            Assert.AreEqual(21, _pf.EndFields[1].Previous.Identifier);
+            Assert.AreEqual(22, _pf.EndFields[2].Previous.Identifier);
+            Assert.AreEqual(23, _pf.EndFields[3].Previous.Identifier);
+        }
+    }
+}
