087c713acad4b07ff9c1ea8e682a79be94b4cdd1
Lukas Steiger <lukas.steiger@outlook.com>
2016-05-24 14:07:02 +0200

    da hed wohl öpper wieder mal Kompilat icheckt! - weg dämit!

--- dog2go/Frontend/Implementation/LoginStartupConfig.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    require(["signalr.hubs"], function () {
-        //new sc.SessionController();
-    });
-});
-//# sourceMappingURL=LoginStartupConfig.js.map
--- dog2go/Frontend/Implementation/Model/TableModel.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=TableModel.js.map
