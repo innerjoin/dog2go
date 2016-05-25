5289e3b4386a75d372d54ed9b91bae26bb69fc8a
jengeler <jengeler@hsr.ch>
2016-05-25 09:03:27 +0200

    Modified Regex to additionally allow äöü and -

--- dog2go/Models/AccountViewModels.cs
+++ dog2go/Models/AccountViewModels.cs
@@ -7,7 +7,7 @@ namespace dog2go.Models
     {
         [Required(ErrorMessage = "You are required to provide a nickname"), StringLength(20, MinimumLength = 3, ErrorMessage = "Must be between 3 and 20 characters")]
         [Display(Name = "User name")]
-        [RegularExpression(@"^[a-zA-Z0-9_''-'\s]*$" ,ErrorMessage = "Invalid character inserted!")]
+        [RegularExpression(@"^[a-zäöüA-Z0-9_\-''-'\s]*$" ,ErrorMessage = "Invalid character inserted!")]
         public string UserName { get; set; }
     }
 
